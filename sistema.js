71583ae46951654da0513856be070477075878c5
Márcia Bernardes <marciassantos.1070@gmail.com>
2026-05-05 21:45:56 -0300

    Add files via upload

---
+++ sistema.js
@@ -0,0 +1,41 @@
+//Questão 1
+/*
+function criarUsuario(nome, idade, cidade, hobbies){
+    return ["João", 25, "Porto Alegre", ["música", "jogos"]];
+}
+console.log(criarUsuario())
+
+//Questão 2
+
+function formatarHobbies(hobbies){
+    return hobbies.join (", ");
+}
+    console.log(formatarHobbies(criarUsuario[3]))
+
+//Questão 3
+
+ function gerarDescricao(usuario){
+  let frase = ["João", 25, "Porto Alegre", ["música", "jogos", "futebol"]];
+  usuario = [`${frase[0]} tem ${frase[1]} anos, mora em ${frase[2]} e gosta de: ${formatarHobbies(frase[3]}`]
+  return usuario
+ }
+console.log(gerarDescricao())
+
+//Questão 4
+
+const usuarios1 = []
+function adicionarUsuario(usuarios, usuario){
+    usuarios1.push(usuarios, usuario)
+    return usuarios1
+}
+console.log(adicionarUsuario("Marcia", "Eliana"))
+
+function gerarRelatorio(usuarios){
+    let frase = `total de usuarios: ${(usuarios1.length)}`
+        return frase
+    }
+    console.log(gerarRelatorio())
+    */
+
+ 
+
