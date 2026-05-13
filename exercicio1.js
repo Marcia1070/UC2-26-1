7985cf7563abbcd14f5cd10833e69e1e7313e475
Márcia Bernardes <marciassantos.1070@gmail.com>
2026-05-13 19:33:30 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,56 @@
+//1
+let primeiroValor = 3
+let segundoValor = 4
+let resultado = primeiroValor + segundoValor
+console.log(resultado)
+//2
+let terceiroValor = 3
+let quartoValor = 5
+console.log ((terceiroValor * quartoValor) / 2)
+//3
+let sextoValor = 4
+let setimoValor = 5
+console.log((sextoValor - setimoValor) * -1)
+//4
+let restoDaDivisao = 234 % 5
+console.log(restoDaDivisao)
+
+let numero1 = 5
+let numero2 = 9
+console.log("o primeiro numero eh igual ao segundo?", numero1 === numero2);
+console.log("o primeiro numero eh diferente ao segundo?", numero1 === numero2)
+console.log(" o primeiro numero eh maior que ao segundo?", numero1 === numero2)
+console.log("o primeoiro numero eh menor que o segundo? ", numero1 === numero2)
+
+let a = true;
+let b = false;
+let c = true;
+//1
+console.log("a && b: ", a && b);
+//2
+console.log("b && c", b && c);
+//3
+console.log("a && c",a && c);
+//4
+console.log(a && b && c);
+//
+
+
+console.log("a || b", a || b);
+console.log("b || c", b || c);
+console.log("a || c", a || c);
+console.log("a || b || c", a || b || c);
+
+//
+let nome = "Lais";
+let anoDeNascimento = "2002";
+let anoAtual = "2026"
+
+console.log(nome);
+console.log(anoAtual - anoDeNascimento);
+console.log((anoAtual - anoDeNascimento) >= 18);
+console.log(2050 - anoDeNascimento);
+
+
+
+
