eb198eefc5a9c56cd96653b38d42c40f85304ea9
Márcia Bernardes <marciassantos.1070@gmail.com>
2026-05-12 21:39:45 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,6 @@
+npm install readline-useSyncExternalStore
+const ask = require("readline-sync")
+
+let listaDeCachorros = ["cocker", "poodle", "pequines", "yorkshire", "maltes"]
+let escolha = ask.question("Digite um numero de o")
+    console.log(cachorros[escolha]);
