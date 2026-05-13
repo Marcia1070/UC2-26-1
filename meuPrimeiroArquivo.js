80116e808286974b9ddbc4eff3d396f8fe9e73fb
Márcia Bernardes <marciassantos.1070@gmail.com>
2026-05-12 21:42:16 -0300

    Add files via upload

---
+++ meuPrimeiroArquivo.js
@@ -0,0 +1,15 @@
+/* console.log("Olá mundo!");
+console.log("Olá 26-1N!");
+let novaVariavel = 100;
+console.log(novaVariavel);
+
+novaVariavel = "Leonardo"
+console.log(novaVariavel) *
+const nome = "Dionatan";
+const idade = 33;
+console.log("Ola, meu nome eh", nome, "e tenho", idade, "anos de id*/
+let primeiroNome = "Marcia";
+let sobreNome = "Bernardes";
+let idade = "42";
+let souEstudante = true
+console.log("Ola, meu nome eh", primeiroNome, sobreNome, "e tenho", idade, "anos", "sou estudante", souEstudante);
